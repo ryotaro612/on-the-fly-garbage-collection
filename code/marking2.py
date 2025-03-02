a4fb969c2be272dc920db01f6bca175720a729a8
Ryotaro <ryotaro612.ooo@gmail.com>
2025-03-02 17:00:12 +0900

    ac

---
+++ code/marking2.py
@@ -0,0 +1,15 @@
+i, k = 0, M
+while k > 0:
+  c = nodes[i].color // atomic
+  if c == GRAY:
+    k = M
+    for side in range(2):
+      succ := nodes[i].successors[side] // atomic
+      // begin atomic
+      if succ.color == WHITE:
+        succ.color = GRAY
+      // end atomic
+      nodes[i].color = BLACK // atomic
+  else:
+    k -= 1
+  i = (i + 1) % M
